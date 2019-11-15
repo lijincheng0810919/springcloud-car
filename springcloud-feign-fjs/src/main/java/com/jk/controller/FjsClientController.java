c53bc472110a642d6dc92170c4f4bb26427aa222
shun484 <shunge321>
2019-11-15 18:43:50 +0800

    二手车前台页面

--- springcloud-feign-fjs/src/main/java/com/jk/controller/FjsClientController.java
+++ springcloud-feign-fjs/src/main/java/com/jk/controller/FjsClientController.java
@@ -1,10 +1,13 @@
 package com.jk.controller;
 
+import com.jk.model.CarBean;
 import com.jk.service.FjsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("fjs")
@@ -13,10 +16,11 @@ public class FjsClientController {
     @Autowired
     private FjsService fjsService;
 
-    @RequestMapping("test")
+    @RequestMapping("findCarBean")
     @ResponseBody
-    private String test(){
-        return fjsService.test();
+    public HashMap<String,Object> findCarBean(Integer page, Integer rows , CarBean carBean){
+        System.out.println(carBean.getCarname());
+        return fjsService.findCarBean(page,rows,carBean);
     }
 
 }
--- springcloud-provider-fjs/src/main/java/com/jk/service/FjsServerService.java
+++ springcloud-provider-fjs/src/main/java/com/jk/service/FjsServerService.java
@@ -6,9 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface FjsServerService {
-    String test();
 
-    void findCarBean(CarBean carBean);
 
     HashMap<String, Object> findCar(Integer page, Integer rows, CarBean carBean);
 }
